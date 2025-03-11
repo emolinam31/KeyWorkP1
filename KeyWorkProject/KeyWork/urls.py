330706a52987cc9c6f024703941b1ca27a7691ca
Esteban Molina Mejia <emolinam@eafit.edu.co>
2025-03-11 11:01:41 -0500

    Desarrollo de requisito funcional FR01 y FR02, para subir y mostrar las diferentes opciones de upload de cv

--- KeyWorkProject/KeyWork/urls.py
+++ KeyWorkProject/KeyWork/urls.py
@@ -1,9 +1,16 @@
+# KeyWorkProject/KeyWork/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),  # Página por defecto
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('CollectionPoint/', include('CollectionPoint.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
