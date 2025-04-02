2f12e2f08ca527a8177f0f0d7c569279865167c4
cadiazh <cadiazh@eafit.edu.co>
2025-04-01 22:33:02 -0500

    Preview de los archivos subidos

---
+++ CollectionPoint/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('upload/', views.upload_cv, name='upload_cv'),
+    path('cv/<int:pk>/', views.cv_detail, name='cv_detail'),
+    path('cv/<int:pk>/process-ocr/', views.process_ocr, name='process_ocr'),
+    
+]
