1419be9483e9056f10c6828bb04e0cd3ae1f6345
Esteban Molina Mejia <emolinam@eafit.edu.co>
2025-04-02 09:47:09 -0500

    Eliminacion de apps: Notifications, Cv_processing

--- KeyWorkProject/KeyWork/urls.py
+++ KeyWorkProject/KeyWork/urls.py
@@ -20,3 +20,5 @@ urlpatterns = [
 # Servir archivos multimedia en desarrollo
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    
--- KeyWorkProject/cv_processing/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CvProcessingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cv_processing'
